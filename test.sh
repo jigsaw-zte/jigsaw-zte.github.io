603bfe0176f0bbfdf94adb96cd4b8ab7a57c80c0
陈旭10045812 <chen.xu8@zte.com.cn>
2020-02-12 23:41:44 +0800

    ...

--- test.sh
+++ test.sh
@@ -1,6 +1,11 @@
+
+
+cookie="JSESSIONID=mrvauzj6mrxbg3e3upws72a9"
 for((i=1;i<5;i++));
 do
-	curl -I -m 10 -o /dev/null -s -w %{http_code} -k -X POST -H "Cookie: JSESSIONID=mrvauzj6mrxbg3e3upws72a9" -d '{"param":{"condition":{"topN":"$i","times":{"beginTime":"2020-02-$i","endTime":"2020-02-$i","granularity":"5"},"filterList":{"filters":[{"fields":[],"filter_type":0}],"filter_type":0}},"paging":{"currentPage":1,"pageSize":10,"totalRecord":0},"isDrill":0,"area":[],"session":"JSESSIONID=mrvauzj6mrxbg3e3upws72a9","currentUserName":"admin"},"app":"vreport"}' https://10.43.149.13:26180/rdk/service/app/vreport/web/rpt_home/sys_lte_traffic_city/server/report_query 
+	curl -k -X POST -H "Cookie: $cookie" https://localhost:26180/rdk/service/app/BigScreenNoc/server/module/getFlowHunnelPieData
+	curl -k -H "Cookie: $cookie" https://localhost:26180/rdk/service/app/vreport/server/isAreaAvailable?p=%7B%22param%22:%7B%7D,%22app%22:%22vreport%22%7D
+	curl -k -X POST -H "Cookie: $cookie" -d '{"param":{"condition":{"topN":"$i","times":{"beginTime":"2020-02-$i","endTime":"2020-02-$i","granularity":"5"},"filterList":{"filters":[{"fields":[],"filter_type":0}],"filter_type":0}},"paging":{"currentPage":1,"pageSize":10,"totalRecord":0},"isDrill":0,"area":[],"session":"$cookie","currentUserName":"admin"},"app":"vreport"}' https://localhost:26180/rdk/service/app/vreport/web/rpt_home/sys_lte_traffic_city/server/report_query 
 	echo "---"
 	# sleep 0.1
 done
